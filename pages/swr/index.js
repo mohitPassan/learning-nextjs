d5d124d38960c1695ffcfe8c111561f3d5b16b8f
Mohit Passan <mohitpassan@gmail.com>
2022-06-17 01:44:52 +0530

    Implemented SWR for orders

--- pages/swr/index.js
+++ pages/swr/index.js
@@ -1,7 +1,49 @@
+import axios from "axios";
+import useSWR from "swr";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
 const SWR = () => {
+    const { data, error } = useSWR("http://localhost:3001/orders", fetcher);
+
+    if (error) {
+        return (
+            <>
+                <h1>There was an error</h1>
+                <p>Error message: {error}</p>
+            </>
+        );
+    }
+
+    if (!data) {
+        return <h1>Loading...</h1>;
+    }
+
     return (
         <div>
-            <h1>SWR</h1>
+            <h1>Orders</h1>
+            {data &&
+                data.map((order) => {
+                    return (
+                        <div key={order.id}>
+                            <p>
+                                <strong>Ordered by: {order.employee}</strong>
+                            </p>
+                            <strong>Products ordered</strong>
+                            <ol>
+                                {order.products.map((product) => {
+                                    return (
+                                        <p key={product.id}>
+                                            ID: {product.id}, Quantity:{" "}
+                                            {product.quantity}
+                                        </p>
+                                    );
+                                })}
+                            </ol>
+                            <hr />
+                        </div>
+                    );
+                })}
         </div>
     );
 };
