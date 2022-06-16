6d0c6d332e88e4e1bb962ac53357dae8e7943fad
Mohit Passan <mohitpassan@gmail.com>
2022-06-17 01:11:42 +0530

    Implemented CSR with orders data

--- pages/csr/index.js
+++ pages/csr/index.js
@@ -1,7 +1,45 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const CSR = () => {
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            const res = await axios.get("http://localhost:3001/orders");
+            const orders = res.data;
+            setOrders(orders);
+        };
+
+        if (orders.length === 0) {
+            fetchOrders();
+        }
+    });
+
     return (
         <div>
-            <h1>CSR</h1>
+            <h1>Orders</h1>
+            {orders.map((order) => {
+                return (
+                    <div key={order.id}>
+                        <p>
+                            <strong>Ordered by: {order.employee}</strong>
+                        </p>
+                        <strong>Products ordered</strong>
+                        <ol>
+                            {order.products.map((product) => {
+                                return (
+                                    <p key={product.id}>
+                                        ID: {product.id}, Quantity:{" "}
+                                        {product.quantity}
+                                    </p>
+                                );
+                            })}
+                        </ol>
+                        <hr />
+                    </div>
+                );
+            })}
         </div>
     );
 };
